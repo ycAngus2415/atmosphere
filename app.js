effb0395fb823c155c85a0e68b293b1121524a67
ycAngus2415 <740274089@qq.com>
2020-02-05 17:53:23 +0800

    add gsap animation

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-import {Menu} from './lib/initJS/CesiumMenu.js';
-import {init} from './lib/initJS/init.js';
-import {Css} from  './lib/initJS/css.js';
-window.fileOptions=  {
-    dataDirectory: false ? 'https://raw.githubusercontent.com/RaymanNg/3D-Wind-Field/master/data/' : 'data/',
-    dataFile: "demo.nc",
-    glslDirectory: false ? '../Cesium-3D-Wind/glsl/' : './lib/windJs/glsl/'
-}
-// const mode = {
-//         debug: demo ? false : true
-//     };
+import {
+	Menu
+} from './lib/initJS/CesiumMenu.js';
+import {
+	init
+} from './lib/initJS/init.js';
+import {
+	Css
+} from './lib/initJS/css.js';
+$(document).ready(function() {
+	window.fileOptions = {
+		dataDirectory: false ? 'https://raw.githubusercontent.com/RaymanNg/3D-Wind-Field/master/data/' : 'data/',
+		dataFile: "demo.nc",
+		glslDirectory: false ? '../Cesium-3D-Wind/glsl/' : './lib/windJs/glsl/'
+	}
+	// const mode = {
+	//         debug: demo ? false : true
+	//     };
 
-var initial = new init();
-var css = new Css();
-var menu = new Menu(initial.viewer);
+	var initial = new init();
+	var css = new Css();
+	var menu = new Menu(initial.viewer);
+});
